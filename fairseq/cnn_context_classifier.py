320fa77b8618996ef3cdf070e11dc4b0911918e3
Seraphina Tarrant <serif@uw.edu>
2020-04-01 05:12:01 -0700

    wrap call to bart in no_grad, also lots of additional debugging tools

--- fairseq/cnn_context_classifier.py
+++ fairseq/cnn_context_classifier.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.autograd as autograd
+import pdb
 
 class CNNContextClassifier(nn.Module):
 
@@ -13,7 +14,9 @@ class CNNContextClassifier(nn.Module):
         self.embedding_dim = bart.model.encoder.embed_tokens.embedding_dim
         self.hidden_dim = hidden_dim
 
-        self.word_embeds = bart.extract_features #nn.Embedding(vocab_size, embedding_dim)
+        self.word_embeds = bart.extract_features 
+        #vocab_size = 50264
+        #self.word_embeds = nn.Embedding(vocab_size, self.embedding_dim).half()
         self.use_cuda = next(bart.parameters()).is_cuda
         # if embed_mat is not None:
         #     self.word_embeds.weight.data = embed_mat
@@ -34,10 +37,11 @@ class CNNContextClassifier(nn.Module):
 
     # vec is seq_len x batch so transpose before iterating over
     def embed_seq(self, vec): 
-    
-        all_embeddings = self.word_embeds(vec.transpose(0, 1)) # results in batch x seq_len x embedsize
-        vec_tr = all_embeddings.transpose(1, 2).contiguous()
-        return vec_tr # dim [batch_size, embed_dim, length]
+        with torch.no_grad():
+            all_embeddings = self.word_embeds(vec.transpose(0, 1)) # results in batch x seq_len x embedsize
+            
+            vec_tr = all_embeddings.transpose(1, 2).contiguous()
+            return vec_tr # dim [batch_size, embed_dim, length]
 
     # Input dimensions: 
     #   context: Tensor dim [seq_len, batch_size].
@@ -46,7 +50,7 @@ class CNNContextClassifier(nn.Module):
     #             dim [batch_size or num_endings] - batch lengths).
     #   Training: num_endings = 1; decoding: batch_size = 1.
     def forward(self, context, endings, itos=None):
-        
+        #pdb.set_trace()
         if type(endings) == tuple: # this is a because sometimes we need tuples for other classifiers, and share a dataloader
             ends = endings[0]
             ends_ls = endings[1]
@@ -64,12 +68,18 @@ class CNNContextClassifier(nn.Module):
 
         maxpool = nn.MaxPool1d(cont_seq_len) # define layer for context length
         #print(context.size(), end.size())
+        #embedding = torch.rand(batch_size, self.embedding_dim, cont_seq_len).half().cuda()
         embedding = self.embed_seq(context)
+        #end_embedding = self.embed_seq(end) # weird memory experiments
+        #return torch.ones(batch_size, requires_grad=True).half().cuda()
+
         context_convol = self.context_conv(embedding)
         context_pooled = maxpool(context_convol).view(batch_size, self.embedding_dim)
          
         maxpool_end = nn.MaxPool1d(end_seq_len)
-        end_conv = F.relu(self.ending_conv(self.embed_seq(end)))
+        #end_embedding = torch.rand(end_batch_size, self.embedding_dim, end_seq_len).half().cuda()
+        end_embedding = self.embed_seq(end)
+        end_conv = F.relu(self.ending_conv(end_embedding))
         end_pooled = maxpool_end(end_conv).view(end_batch_size, self.embedding_dim)
 
         if decode_mode:
@@ -78,5 +88,6 @@ class CNNContextClassifier(nn.Module):
 
         dropped = self.drop(pooled)
         final = self.fc(dropped).view(-1)
+        
         return final
 
