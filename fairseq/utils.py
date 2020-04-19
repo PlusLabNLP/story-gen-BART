2bc6d67d6215626372f1343bf877e5f307b5af3c
Seraphina Tarrant <serif@uw.edu>
2020-04-19 15:56:57 -0700

    messy workaround for loading in scorers

--- fairseq/utils.py
+++ fairseq/utils.py
@@ -1,8 +1,9 @@
 import sys
 from importlib import import_module
+import torch
+from cnn_context_classifier import CNNContextClassifier
 
-
-def load_scorers(filepath, cuda=False):
+def load_scorers(filepath, bart, cuda=False):
     scorer_config, scorers, coefs = [], [], []
     print("Creating scorers", file=sys.stderr)
     with open(filepath) as scorer_file:
@@ -14,7 +15,10 @@ def load_scorers(filepath, cuda=False):
             model_location = fields[3]
             module = import_module(module_path)
             constructor = getattr(module, classname)
-            scorer = constructor(model_location, cuda)
+            model = CNNContextClassifier(300, 3, 0.5, bart)
+            x = torch.load(model_location)
+            model.load_state_dict(x)
+            scorer = constructor(model, cuda)
             scorers.append(scorer)
             coefs.append(weight)
     print("Coefs:", coefs, file=sys.stderr)
