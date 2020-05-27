4cbba587c99f7d301c25884b02ddd655023d2b94
Seraphina Tarrant <serif@uw.edu>
2020-05-27 05:18:48 -0700

    add new concat titles function option

--- fairseq/postprocess.py
+++ fairseq/postprocess.py
@@ -13,6 +13,8 @@ def setup_argparse():
     p.add_argument('-d', dest='input_dir')
     p.add_argument('-f', dest='files', nargs='+')
     p.add_argument('--detokenize', action='store_true')
+    p.add_argument('--concat_titles', action='store_true')
+    p.add_argument('--titles', type=str, help="path to title file if concat titles")
     p.add_argument('--remove_partial', action='store_true',
                    help="if True removes partial sentences that were truncated in generation")
     p.add_argument('--sent_sym', default='</s>', type=str, help='if removing partial sentences, '
@@ -43,6 +45,15 @@ def make_human_readable(files: list, detokenize: bool,
                 fout.write("{}\n".format(cleanline))
 
 
+def concat_title_plot(title_file, plot_files):
+    with open(title_file, "r") as t_fin:
+        all_titles = t_fin.readlines()
+    for file in plot_files:
+        with open(file, "r") as fin, open(file+".title+plot", "w") as fout:
+            for i, line in enumerate(fin):
+                fout.write("{} <EOT> {}\n".format(all_titles[i].strip(), line.strip()))
+
+
 if __name__ == "__main__":
     args = setup_argparse()
 
@@ -53,7 +64,10 @@ if __name__ == "__main__":
     else:
         files = args.files
 
-    make_human_readable(files, detokenize=args.detokenize,
+    if args.concat_titles:
+        concat_title_plot(args.titles, files)
+    else:
+        make_human_readable(files, detokenize=args.detokenize,
                         remove_partial_sent=args.remove_partial, sent_sym=args.sent_sym)
 
 
