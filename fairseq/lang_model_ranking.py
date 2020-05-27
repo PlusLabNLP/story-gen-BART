8adf18088d87dc3d83dc9058c77c737e39745d4c
seraphinatarrant <serif@uw.edu>
2020-05-27 13:57:25 +0100

    add args.both and also support for gold story metrics

--- fairseq/lang_model_ranking.py
+++ fairseq/lang_model_ranking.py
@@ -11,6 +11,7 @@ def setup_argparse():
     p = argparse.ArgumentParser()
     p.add_argument('--data_type', type=str, choices=["plot", "story"])
     p.add_argument('--ranking', action="store_true", help="output ranking accuracy of x vs y pairs")
+    p.add_argument('--both', action="store_true", help="output both ranking accuracy and mean scores")
     p.add_argument('--gen_data', nargs="+", help="generated files")
     p.add_argument('--cond_data', type=str, help="filename of conditional data")
     p.add_argument('--gold', type=str, help="gold stories, necessary for ranking accuracy")
@@ -47,6 +48,12 @@ if __name__ == "__main__":
     if args.ranking:
         with open(args.gold, "r") as fin:
             gold_stories = fin.readlines()
+            if args.data_type == "story":  # story probabilities are the intermediate representation as well as the story rep
+                gold_titles, gold_plots = [], []
+                for line in gold_stories:
+                    gold_title, gold_plot = line.strip().split(title_plot_sep)
+                    gold_titles.append(gold_title)
+                    gold_plots.append(gold_plot)
 
     with open(args.cond_data, "r") as fin:
         cond_data = fin.readlines()
@@ -62,7 +69,7 @@ if __name__ == "__main__":
         print("Working on {}...".format(file))
         # run sequence scorer
         gold_win, gen_win = 0, 0
-        all_scores = []
+        all_scores, all_gold_scores = [], []
         with open(file, "r") as fin:
             for i, line in enumerate(fin):
                 with torch.no_grad():
@@ -71,6 +78,7 @@ if __name__ == "__main__":
                     all_scores.append(score)
                     if args.ranking:
                         gold_score = bart.score_sequence([cond_data[i].strip()], [gold_stories[i].strip()])
+                        all_gold_scores.append(gold_score)
                         if gold_score > score:
                             gold_win += 1
                         else:
@@ -81,15 +89,30 @@ if __name__ == "__main__":
                 with torch.no_grad():
                     score = bart.score_sequence([titles[i].strip()], [plots[i].strip()])
                     plot_scores.append(score)
+            if args.ranking or args.both:
+                gold_plot_scores = []
+                for i in range(len(gold_titles)):
+                    with torch.no_grad():
+                        gold_score = bart.score_sequence([gold_titles[i].strip()], [gold_plots[i].strip()])
+                        gold_plot_scores.append(gold_score)
 
-        if args.ranking:
+
+        if args.ranking or args.both:
             print("Generated ranked above gold: {:.2f}".format(gen_win/(gold_win+gen_win)))
 
-        else:
             if args.data_type == "story":
-                print("Mean probability of plots and of stories respectively: "
-                      "{:.2f} {:.2f}".format(np.mean(plot_scores), np.mean(all_scores)))
-            print("Mean conditional probability of sequences: {:.2f}".format(np.mean(all_scores)))
+                print("Mean probability of gold plots and of stories respectively: "
+                      "{:.2f} {:.2f}".format(np.mean(gold_plot_scores), np.mean(all_gold_scores)))
+            else:
+                print("Mean conditional probability of gold sequences: {:.2f}".format(
+                    np.mean(all_gold_scores)))
+
+
+        if args.data_type == "story":
+            print("Mean probability of generated plots and of stories respectively: "
+                  "{:.2f} {:.2f}".format(np.mean(plot_scores), np.mean(all_scores)))
+        else:
+            print("Mean conditional probability of generated sequences: {:.2f}".format(np.mean(all_scores)))
 
 
 
