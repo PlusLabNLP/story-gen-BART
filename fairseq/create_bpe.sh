cbb280064088f0ed350e32c7adbe4d3ce1bf34d8
Manuel R. Ciosici <manuelrciosici@gmail.com>
2020-11-10 14:08:17 -0500

    Support command line argument for BPE creation

--- fairseq/create_bpe.sh
+++ fairseq/create_bpe.sh
@@ -1,3 +1,6 @@
+# Run with sh create_bpe.sh directory
+dir=$1
+
 for SPLIT in train val
 do
   for LANG in source target
@@ -5,8 +8,8 @@ do
     python -m examples.roberta.multiprocessing_bpe_encoder \
     --encoder-json encoder.json \
     --vocab-bpe vocab.bpe \
-    --inputs "temp/$SPLIT.$LANG" \
-    --outputs "temp/$SPLIT.bpe.$LANG" \
+    --inputs "$dir/$SPLIT.$LANG" \
+    --outputs "$dir/$SPLIT.bpe.$LANG" \
     --workers 60 \
     --keep-empty;
   done
